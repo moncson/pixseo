2e800b663a08cc5e629a7e2c3c400c5b803bf2b2
moncson <moncson@gmail.com>
2025-11-15 16:25:10 +0900

    fix: ライターページのCategoryBarプロパティを修正
    🐛 修正内容
    
    - currentCategorySlug プロパティを削除
    - CategoryBarコンポーネントは categories のみを受け取る

--- app/media/writers/[id]/page.tsx
+++ app/media/writers/[id]/page.tsx
@@ -140,7 +140,6 @@ export default async function WriterPage({ params }: PageProps) {
       {/* カテゴリーバー */}
       <CategoryBar 
         categories={headerCategories}
-        currentCategorySlug=""
       />
 
       {/* メインコンテンツ */}
